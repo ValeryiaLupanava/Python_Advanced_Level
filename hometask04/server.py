59391d22a9164faff33f63913e4dc35479e6f3f8
valeryia_lupanava <33247200+ValeryiaLupanava@users.noreply.github.com>
2019-11-21 13:31:54 +0300

    hometask04
    Valeryia Lupanava

---
+++ hometask04/server.py
@@ -0,0 +1,98 @@
+"""
+Программа-сервер.
+Функции сервера:
+1. принимает сообщение клиента;
+2. формирует ответ клиенту;
+3. отправляет ответ клиенту;
+4. имеет параметры командной строки:
+    -p <port> — TCP-порт для работы (по умолчанию использует 7777);
+    -a <addr> — IP-адрес для прослушивания (по умолчанию слушает все доступные адреса).
+"""
+
+import socket
+import sys
+import json
+from lesson03.common.variables import ACTION, RESPONSE, \
+    MAX_CONNECTIONS, PRESENCE, TIME, USER, ERROR, DEFAULT_PORT, \
+    RESPONDEFAULT_IP_ADDRESSSE, IP_ADDRESS
+from lesson03.common.utils import get_message, send_message
+
+
+def process_client_message(message):
+    '''
+    Обработчик сообщений от клиентов, принимает словарь -
+    сообщение от клинта, проверяет корректность,
+    возвращает словарь-ответ для клиента
+
+    :param message:
+    :return:
+    '''
+    if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
+            and USER in message and message[IP_ADDRESS] == '192.168.0.100':
+        return {RESPONSE: 200}
+    return {
+        RESPONDEFAULT_IP_ADDRESSSE: 400,
+        ERROR: 'Bad Request'
+    }
+
+
+def main():
+    '''
+    Загрузка параметров командной строки, если нет параметров, то задаём значения по умоланию.
+    Сначала обрабатываем порт:
+    server.py -p 8888 -a 127.0.0.1
+    :return:
+    '''
+
+    try:
+        if '-p' in sys.argv:
+            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
+        else:
+            listen_port = DEFAULT_PORT
+        if listen_port < 1024 or listen_port > 65535:
+            raise ValueError
+    except IndexError:
+        print('После параметра -\'p\' необходимо указать номер порта.')
+        sys.exit(1)
+    except ValueError:
+        print(
+            'В качастве порта может быть указано только число в диапазоне от 1024 до 65535.')
+        sys.exit(1)
+
+    # Затем загружаем какой адрес слушать
+
+    try:
+        if '-a' in sys.argv:
+            listen_address = sys.argv[sys.argv.index('-a') + 1]
+        else:
+            listen_address = ''
+
+    except IndexError:
+        print(
+            'После параметра \'a\'- необходимо указать адрес, который будет слушать сервер.')
+        sys.exit(1)
+
+    # Готовим сокет
+
+    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    transport.bind((listen_address, listen_port))
+
+    # Слушаем порт
+
+    transport.listen(MAX_CONNECTIONS)
+
+    while True:
+        client, client_address = transport.accept()
+        try:
+            message_from_clnt = get_message(client)
+            print(message_from_clnt)
+            response = process_client_message(message_from_clnt)
+            send_message(client, response)
+            client.close()
+        except (ValueError, json.JSONDecodeError):
+            print('Принято некорретное сообщение от клиента.')
+            client.close()
+
+
+if __name__ == '__main__':
+    main()
